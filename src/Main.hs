a84b66e62892cdf80bd30d8113ad4e42a20602ad
Johan Sandgren <gussanjoba@student.gu.se>
2024-03-22 17:22:52 +0100

    parsec init, AST generation for simple variables

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,44 @@
 module Main (main) where
 
+import Text.Parsec
+import Text.Parsec.Language (emptyDef)
+import Text.Parsec.String (Parser)
+import qualified Text.Parsec.Token as Tok
+
+data Type = IntegerType deriving (Show)
+
+data Expr
+  = IntLiteral Integer
+  | VarDecl String Type Expr
+  deriving (Show)
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+  where
+    ops = []
+    names = []
+    style =
+      emptyDef
+        { Tok.reservedOpNames = ops,
+          Tok.reservedNames = names,
+          Tok.commentLine = "#"
+        }
+
+integer :: Parser Expr
+integer = IntLiteral . fromIntegral <$> Tok.integer lexer
+
+integerType :: Parser Type
+integerType = Tok.symbol lexer ":: Integer" >> return IntegerType
+
+varDecl :: Parser Expr
+varDecl = do
+  varName <- Tok.identifier lexer
+  integerType
+  value <- Tok.symbol lexer "=" >> integer
+  return $ VarDecl varName IntegerType value
+
+parseTestVar :: String -> Either ParseError Expr
+parseTestVar input = parse varDecl "" input
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = print $ parseTestVar "x :: Integer = 5"
